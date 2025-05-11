9654c7682105bc89d4f3ca10f643edc887f7290c
ORLAN07 <orlandorivasrubio@live.com>
2025-05-11 17:14:36 -0500

    configure mf simulator

--- mf-workspace/projects/mf-simulator/src/app/app.routes.ts
+++ mf-workspace/projects/mf-simulator/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import("./default/default.component").then((m) => m.DefaultComponent)
+    },
+    {
+        path: 'simulator',
+        loadComponent: () => import("./simulator/simulator.component").then((m) => m.SimulatorComponent)
+    }
+];
