47aba9888e881883a69bde09edbc443f4b034d02
ORLAN07 <orlandorivasrubio@live.com>
2025-05-11 21:37:08 -0500

    pending connect host with remote

--- mf-workspace/projects/host/src/app/app.routes.ts
+++ mf-workspace/projects/host/src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent)
+    },
+    {
+        path: 'mf-simulator',
+        loadChildren: () => import('mf-simulator/AppComponent').then((m) => m.AppComponent)
     }
 ];
